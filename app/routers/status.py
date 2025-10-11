e78c2df5206d08746926665c4e81ec9617303661
mmarci96 <sarosdimarci@gmail.com>
2025-10-11 21:21:23 +0200

    move olda files to server dir

--- app/routers/status.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.service.BotService import bot_service
-
-router = APIRouter(prefix="/api/status", tags=["Status"])
-
-
-@router.get("/")
-def status():
-    return bot_service.get_status()
